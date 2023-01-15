7b45199a5ccc392ee77eb0010e3c60dde630c119
gdhyani <gauravdhyani90@gmail.com>
2023-01-16 00:59:46 +0530

    control snake

--- main.py
+++ main.py
@@ -4,13 +4,60 @@ screen = Screen()
 screen.bgcolor('black')
 screen.setup(500, 500)
 
-
+# snake on screen
 snake = Turtle('square')
+
 length = 3
 snake.shapesize(1, length)
 snake.penup()
 snake.fillcolor('white')
 
+# control snake
 
 
+def move_up():
+    print(snake.heading())
+    if snake.heading() == 0:
+        snake.left(90)
+    elif snake.heading() == 180:
+        snake.right(90)
+    else:
+        pass
+
+
+def move_down():
+    print(snake.heading())
+    if snake.heading() == 0:
+        snake.right(90)
+    elif snake.heading() == 180:
+        snake.left(90)
+    else:
+        pass
+
+
+def move_left():
+    print(snake.heading())
+    if snake.heading() == 90:
+        snake.left(90)
+    elif snake.heading() == 270:
+        snake.right(90)
+    else:
+        pass
+
+
+def move_right():
+    print(snake.heading())
+    if snake.heading() == 90:
+        snake.right(90)
+    elif snake.heading() == 270:
+        snake.left(90)
+    else:
+        pass
+
+
+screen.listen()
+screen.onkey(move_up, "w")
+screen.onkey(move_down, 's')
+screen.onkey(move_left, 'a')
+screen.onkey(move_right, 'd')
 screen.exitonclick()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-screen.exitonclick()
+snake.listen()
+snake.onkey(move_up, "w")
+snake.onkey(move_down, 's')
+snake.onkey(move_left, 'a')
+snake.onkey(move_right, 'd')
